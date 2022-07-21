0f3bf0741a9394c6c4444ac42c2c111cbf3be710
Jiwoon2 <dkswjsgkek@naver.com>
2022-07-22 00:39:06 +0900

    Firebase 사진 공유 앱
    Firebase 연동, 회원가입

---
+++ 08 PublicGalleryJW/screens/RootStack.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import SignInScreen from './SignInScreen';
+import WelcomeScreen from './WelcomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="SignIn"
+        component={SignInScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default RootStack;
